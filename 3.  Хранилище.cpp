1bb7d8b3865460aa4e03c1fd29d42e8dcd1f226d
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-22 20:05:56 +0300

    Add files via upload

---
+++ 3.  Хранилище.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> db;
+    int num = 0;
+    int count = 0;
+ 
+    while (num != -1) {  
+        std::cout << "Enter your number № " << count + 1 << " : ";
+        std::cin >> num;
+        if (num == -1) {
+            break;
+        }
+        db.push_back(num);
+        count++;
+        if (count > 20) {
+            db.erase(db.begin());
+        }
+    }
+
+    for (int i = 0; i < db.size(); i++) {
+        std::cout << db[i] << " ";
+    }
+}
