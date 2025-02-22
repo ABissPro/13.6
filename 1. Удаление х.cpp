4a9c91e103bf31cf105a11ce9efa4b95ef79858a
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-22 20:21:28 +0300

    Add files via upload

---
+++ 1. Удаление х.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int n;
+    std::cout << "Введите длину вектора: ";
+    std::cin >> n;
+    std::vector<int> vec(n);
+    std::cout << "Введите " << n << " целых чисел: ";
+    for (int i = 0; i < n; i++) {
+        std::cin >> vec[i];
+    }
+    int x;
+    std::cout << "Введите значение х для удаления его из вектора: ";
+    std::cin >> x;
+    int count = 0;
+    int i = 0;
+    while (i < vec.size()) {
+        if (vec[i] == x) {
+            int del = vec[i];
+            vec.erase(vec.begin() + i);
+            //count++;
+            //vec[i - count] = vec[i];
+        }
+        i++;
+    }
+    //vec.resize(vec.size() - count);
+    std::cout << "Результат: ";
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
+
+}
+
